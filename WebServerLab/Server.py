b4cebee7dc35675c8aa43fc46419d5a99e788784
Thuy Luu <thuy.luu@sjsu.edu>
2021-03-26 22:30:20 -0700

    Commit for Web Server Lab

---
+++ WebServerLab/Server.py
@@ -0,0 +1,40 @@
+# import socket module
+from socket import *
+import sys  # In order to terminate the program
+
+serverHost = '192.168.1.35'
+serverPort = 1234
+serverSocket = socket(AF_INET, SOCK_STREAM)
+# Prepare a server socket
+serverSocket.bind((serverHost, serverPort))
+serverSocket.listen(1)
+
+while True:
+    # Establish the connection
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+    try:
+        message = connectionSocket.recv(1024)
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = f.read()
+        # Send one HTTP header line into socket
+
+        connectionSocket.send(b"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n")
+        connectionSocket.send(b"\r\n")
+        # Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        connectionSocket.send("\r\n".encode())
+        connectionSocket.close()
+    except IOError:
+        # Send response message for file not found
+
+        connectionSocket.send(b"HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n")
+        connectionSocket.send(b"<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n")
+        # Close client socket
+        connectionSocket.close()
+serverSocket.close()
+sys.exit()  # Terminate the program after sending the corresponding data
+
+
